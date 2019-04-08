e645641eb41c2d3be8775c3bb62f4ade9d707400
Artem Zhivaev <zhivaev993@gmail.com>
2019-04-08 21:20:57 +0400

    test2

--- modules/complex-number/test/test_fio.cpp
+++ modules/complex-number/test/test_fio.cpp
@@ -14,3 +14,16 @@ TEST(FIO_ComplexNumberTest, Success_Test) {
     EXPECT_EQ(re, z.getRe());
     EXPECT_EQ(im, z.getIm());
 }
+
+TEST(FIO_ComplexNumberTest, Success_Test_1) {
+    // Arrange
+    double re = 0.0;
+    double im = 0.0;
+    bool res = false;
+
+    // Act
+    ComplexNumber z(re, im);
+
+    // Assert
+    EXPECT_EQ(false, res);
+}
