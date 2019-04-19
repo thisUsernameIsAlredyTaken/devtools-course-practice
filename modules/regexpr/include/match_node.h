32da04893f074f6f611edcd1290b71297e824f2f
Zhivaev Artem <zhivaev993@gmail.com>
2019-04-19 06:52:03 +0300

    match_node.h updated

--- modules/regexpr/include/match_node.h
+++ modules/regexpr/include/match_node.h
@@ -3,55 +3,43 @@
 
 #include <string>
 #include <set>
+#include <vector>
 
-/*
- * Char class ([abc])             V
- * Any count of chars ([abc]*)    V
- * One or zero chars ([abc]?)     V
- * Some chars ([abc]{1,5})        V
- * Group ((abc|defg))             X
- */
-
-class MatchNode {
-public:
-    virtual bool match(std::string::const_iterator &it) const = 0;
+enum Modif {
+    kNone,
+    kSingle,
+    kAsterisk,
+    kQMark,
+    kRange,
 };
 
-class MN_CharSetSingle : public MatchNode {
-public:
-    MN_CharSetSingle(const std::set<char> &s);
-    MN_CharSetSingle(const std::string &s);
-    MN_CharSetSingle(const char *s);
+class Matcher {
+ public:
+    virtual bool match(std::string::const_iterator &it) const = 0;
+    void setNext(Matcher*);
+    Matcher();
+    virtual ~Matcher();
+ protected:
+    Matcher *next_;
+    Modif mod_;
+    int min_, max_;
+};
+
+class MCharSet : public Matcher {
+ public:
     virtual bool match(std::string::const_iterator &it) const override;
-protected:
+    MCharSet(const std::string&);
+ protected:
     std::set<char> chSet_;
 };
 
-class MN_CharSetAnyCount : public MN_CharSetSingle {
-public:
-    MN_CharSetAnyCount(const std::set<char> &s);
-    MN_CharSetAnyCount(const std::string &s);
-    MN_CharSetAnyCount(const char *s);
+class MGroup : public Matcher {
+ public:
     virtual bool match(std::string::const_iterator &it) const override;
-};
-
-class MN_CharSetOneOrZero : public MN_CharSetSingle {
-public:
-    MN_CharSetOneOrZero(const std::set<char> &s);
-    MN_CharSetOneOrZero(const std::string &s);
-    MN_CharSetOneOrZero(const char *s);
-    virtual bool match(std::string::const_iterator &it) const override;
-};
-
-class MN_CharSetCertainCount : public MN_CharSetSingle {
-public:
-    MN_CharSetCertainCount(const std::set<char> &s);
-    MN_CharSetCertainCount(const std::string &s);
-    MN_CharSetCertainCount(const char *s);
-    virtual bool match(std::string::const_iterator &it) const override;
-private:
-    int minCount_;
-    int maxCount_;
+    MGroup(const std::string&);
+    ~MGroup();
+ protected:
+    std::vector<Matcher*> members_;
 };
 
 #endif // MODULES_REGEXPR_INCLUDE_MATCH_NODE_H_
--- modules/regexpr/src/match_node.cpp
+++ modules/regexpr/src/match_node.cpp
@@ -1,67 +1,47 @@
 #include "include/match_node.h"
 
-using std::set;
 using std::string;
 
-MN_CharSetSingle::MN_CharSetSingle(const string &s) {
-    for (auto i : s) {
-        chSet_.insert(i);
+void Matcher::setNext(Matcher *next) {
+    next_ = next;
+}
+
+Matcher::Matcher()
+    : next_(nullptr), mod_(Modif::kNone), min_(-1), max_(-1) {}
+
+Matcher::~Matcher() {}
+
+
+
+bool MCharSet::match(string::const_iterator &it) const {
+    /*
+     * TODO
+     */
+}
+
+MCharSet::MCharSet(const string &s) {
+    /*
+     * TODO
+     */
+}
+
+
+
+bool MGroup::match(string::const_iterator &it) const {
+    /*
+     * TODO
+     */
+}
+
+MGroup::MGroup(const string &s) {
+    /*
+     * TODO
+     */
+}
+
+MGroup::~MGroup() {
+    for (auto i : members_) {
+        delete i;
     }
-}
-
-MN_CharSetSingle::MN_CharSetSingle(const set<char> &s) {
-    chSet_ = s;
-}
-
-MN_CharSetSingle::MN_CharSetSingle(const char *s) {
-    const char *p;
-
-    for (p = s; *p != '\0'; ++p) {
-        chSet_.insert(*p);
-    }
-}
-
-bool MN_CharSetSingle::match(string::const_iterator &it) const {
-    if (chSet_.find(*it) != chSet_.end()) {
-        ++it;
-        return true;
-    }
-    return false;
-}
-
-
-
-
-MN_CharSetAnyCount::MN_CharSetAnyCount(const set<char> &s)
-    : MN_CharSetSingle(s) {}
-
-MN_CharSetAnyCount::MN_CharSetAnyCount(const string &s)
-    : MN_CharSetSingle(s) {}
-
-MN_CharSetAnyCount::MN_CharSetAnyCount(const char *s)
-    : MN_CharSetSingle(s) {}
-
-bool MN_CharSetAnyCount::match(string::const_iterator &it) const {
-    while (chSet_.find(*it) != chSet_.end()) {
-        ++it;
-    }
-    return true;
-}
-
-
-
-
-MN_CharSetOneOrZero::MN_CharSetOneOrZero(const set<char> &s)
-    : MN_CharSetOneOrZero(s) {}
-
-MN_CharSetOneOrZero::MN_CharSetOneOrZero(const string &s)
-    : MN_CharSetOneOrZero(s) {}
-
-MN_CharSetOneOrZero::MN_CharSetOneOrZero(const char *s)
-    : MN_CharSetOneOrZero(s) {}
-
-bool MN_CharSetOneOrZero::match(string::const_iterator &it) const {
-    ++it;
-    return false;
 }
 
