c68c6564d494e5dca9ab7fcd10e33b4cb4ecf79c
Zhivaev Artem <zhivaev993@gmail.com>
2019-04-19 17:28:39 +0300

    new tests for matcher

--- modules/regexpr/test/test_matcher.cpp
+++ modules/regexpr/test/test_matcher.cpp
@@ -2,12 +2,19 @@
 
 #include <gtest/gtest.h>
 
+#define protected public
+#define private   public
+
 #include "include/matcher.h"
+
+#undef private
+#undef protected
+
 
 TEST(MatcherTest, MCharSet_Moves_Iterator) {
     // Arrange
-    std::string seq("abc");
-    std::string::const_iterator it = seq.begin();
+    const std::string seq("abc");
+    auto it = seq.begin();
 
     // Act
     MCharSet m(it);
@@ -40,4 +47,75 @@ TEST(MatcherTest, MCharSet_Moves_Iterator_Char_Class_Range) {
     // Assert
     EXPECT_EQ(std::next(seq.begin(), 5), it);
 }
+
+TEST(MatcherTest, MCharSet_Single_Char) {
+    // Arrange
+    const std::string seq("abc");
+    auto it = seq.begin();
+    std::set<char> s;
+
+    // Act
+    MCharSet m(it);
+    s.insert('a');
+
+    // Assert
+    EXPECT_EQ(s, m.chSet_);
+}
+
+TEST(MatcherTest, MCharSet_CharClass) {
+    // Arrange
+    const std::string seq("[abc]defg");
+    auto it = seq.begin();
+    std::set<char> s;
+
+    // Act
+    MCharSet m(it);
+    s.insert('a');
+    s.insert('b');
+    s.insert('c');
+
+    // Assert
+    EXPECT_EQ(s, m.chSet_);
+}
+
+TEST(MatcherTest, MCharSet_Range_CharClass) {
+    // Arrange
+    const std::string seq("[a-f]ghij");
+    auto it = seq.begin();
+    std::set<char> s;
+
+    // Act
+    MCharSet m(it);
+    s.insert('a');
+    s.insert('b');
+    s.insert('c');
+    s.insert('d');
+    s.insert('e');
+    s.insert('f');
+
+    // Assert
+    EXPECT_EQ(s, m.chSet_);
+}
+
+TEST(MatcherTest, MCharSet_Mixed_CharClass) {
+    // Arrange
+    const std::string seq("[a-def]ghij");
+    auto it = seq.begin();
+    std::set<char> s;
+
+    // Act
+    MCharSet m(it);
+    s.insert('a');
+    s.insert('b');
+    s.insert('c');
+    s.insert('d');
+    s.insert('e');
+    s.insert('f');
+
+    // Assert
+    EXPECT_EQ(s, m.chSet_);
+}
+
+
+
 
